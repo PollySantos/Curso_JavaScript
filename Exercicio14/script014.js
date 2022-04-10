7e683a78a0d89906e3cd55c6ec134ed0f91c8870
Polly Santos <99842806+PollySantos@users.noreply.github.com>
2022-04-10 16:17:05 -0300

    Learning JS

--- Exercicio14/script014.js
+++ Exercicio14/script014.js
@@ -5,10 +5,13 @@ function carregar() {
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas. `
     if (hora >= 0 && hora < 12) {
-       img.src = 'dia.png' 
+       img.src = './img/dia.png'
+       document.body.style.background = '#8f872a'
     } else if (hora >= 12 && hora < 18) {
-        img.src = 'tarde.png'
+        img.src = './img/tarde.png'
+        document.body.style.background = '#e28743'
     } else {
-        img.src = 'noite.png'
+        img.src = './img/noite.png'
+        document.body.style.background = '#1c2222'
     }
 }
