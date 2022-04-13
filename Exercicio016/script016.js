2d4bc0d075a741d7a7507bfc8d9aa5dacfc7d88b
Polly Santos <99842806+PollySantos@users.noreply.github.com>
2022-04-13 18:40:44 -0300

    JS

---
+++ Exercicio016/script016.js
@@ -0,0 +1,14 @@
+var c  = 1
+while (c <= 3) {
+    console.log(`passo ${c}`)
+    c++
+}
+
+// estrutura while acima
+// estrutura do while abaixo
+
+var c  = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 6)
